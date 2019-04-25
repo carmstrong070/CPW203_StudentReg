0c281683993244ed5d4847e67ce60d2c8d3a375a
CHRISTIAN ARMSTRONG <armstrong2657@students.cptc.edu>
2019-04-25 15:01:50 -0700

    Added validation and register functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,58 @@
 var Student = /** @class */ (function () {
-    function Student(fName, lName, dob) {
-        this.firstName = fName;
-        this.lastName = lName;
-        this.dateOfBirth = dob;
+    function Student() {
     }
     return Student;
 }());
 // Test code
-var testStu = new Student("f", "l", new Date(2019, 4, 18));
+var testStu = new Student();
+testStu.firstName = "First";
+testStu.lastName = "Last";
+testStu.dateOfBirth = new Date(1996, 11, 7);
 window.onload = function () {
     var regBtn = document.querySelector("main > button");
     regBtn.onclick = registerStudent;
 };
 function registerStudent() {
-    var newStudent = getStudent();
-    displayStudent(newStudent);
+    if (isValid()) {
+        var nextStudent = getStudent();
+        displayStudent(nextStudent);
+    }
+}
+function isValid() {
+    // validate all required elements
+    var reqElems = document.querySelectorAll("main > input[data-required]");
+    reqElems.forEach(function (element) {
+        if (element.value == "") {
+            var span = element.nextElementSibling;
+            var msg = span.getAttribute("data-msg");
+            span.innerHTML = msg;
+            return false;
+        }
+    });
+    return true;
 }
 function getStudent() {
     var fnameInput = document.getElementById("txt_firstName");
-    var lnameInput = document.getElementById("txt_lasstName");
+    var lnameInput = document.getElementById("txt_lastName");
     //let dobInput: HTMLInputElement = <HTMLInputElement> document.getElementById("txt_dob");
-    var stu = new Student(fnameInput.value, lnameInput.value, new Date());
-    return testStu;
+    var stu = new Student();
+    stu.firstName = fnameInput.value;
+    stu.lastName = lnameInput.value;
+    return stu;
 }
 function displayStudent(stu) {
+    //<li> </li>
+    var studentLI = document.createElement("li");
+    //<li>J. Doe</li>
+    studentLI.innerText = stu.firstName + " " + stu.lastName;
+    studentLI.onclick = function () {
+        var agree = confirm("Are you sure you want delete?");
+        if (agree) {
+            var currItem = this;
+            currItem.remove();
+        }
+    };
+    //getting <ul> and appending the new <li>
+    var list = document.querySelector("#roster > ul");
+    list.appendChild(studentLI);
 }
--- typescript/main.ts
+++ typescript/main.ts
@@ -2,17 +2,13 @@ class Student {
     firstName: string;
     lastName: string;
     dateOfBirth: Date;
-
-    constructor (fName: string, lName: string, dob: Date){
-        this.firstName = fName;
-        this.lastName = lName;
-        this.dateOfBirth = dob;
-    }
-
 }
-// Test code
 
-let testStu: Student = new Student("f", "l", new Date(2019, 4, 18));
+// Test code
+let testStu: Student = new Student();
+testStu.firstName = "First";
+testStu.lastName = "Last";
+testStu.dateOfBirth = new Date(1996, 11, 7);
 
 window.onload = function(){
     let regBtn  = <HTMLElement> document.querySelector("main > button");
@@ -20,22 +16,53 @@ window.onload = function(){
 }
 
 function registerStudent(){
-    let newStudent: Student = getStudent(); 
-    displayStudent(newStudent);
+    if (isValid()) {
+       let nextStudent: Student = getStudent(); 
+        displayStudent(nextStudent); 
+    }
+}
+
+function isValid(){
+    // validate all required elements
+    let reqElems = document.querySelectorAll("main > input[data-required]");
+    
+    reqElems.forEach(element => {
+        if ((<HTMLInputElement>element).value == "") {
+            let span = element.nextElementSibling;
+            let msg = span.getAttribute("data-msg");
+            span.innerHTML = msg;
+            return false;
+        }
+    });
+
+    return true;
 }
 
 function getStudent(): Student{
     let fnameInput: HTMLInputElement = <HTMLInputElement> document.getElementById("txt_firstName");
-    let lnameInput: HTMLInputElement = <HTMLInputElement> document.getElementById("txt_lasstName");
+    let lnameInput: HTMLInputElement = <HTMLInputElement> document.getElementById("txt_lastName");
     //let dobInput: HTMLInputElement = <HTMLInputElement> document.getElementById("txt_dob");
-    let stu: Student = new Student(fnameInput.value, lnameInput.value, new Date());
-
-    return testStu;
+    let stu: Student = new Student();
+    stu.firstName = fnameInput.value;
+    stu.lastName = lnameInput.value;
+    return stu;
 }
 
 function displayStudent(stu: Student){
-    
+    //<li> </li>
+    let studentLI:HTMLLIElement = document.createElement("li");
+    //<li>J. Doe</li>
+    studentLI.innerText = stu.firstName + " " + stu.lastName;
 
-    /*let list = document.querySelector("#roster > ul")
-    list.appendChild(studentLI)*/
+    studentLI.onclick = function(){
+        let agree = confirm("Are you sure you want delete?");
+        if(agree){
+            let currItem = <HTMLLIElement>this;
+            currItem.remove();
+        }
+    }
+    
+    //getting <ul> and appending the new <li>
+    let list = document.querySelector("#roster > ul");
+    list.appendChild(studentLI);
 }
